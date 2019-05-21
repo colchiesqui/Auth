318cbc741ec182890f7377117d3fbdb9cbb47f6c
Pablo <pablocolchiesqui@gmail.com>
2019-05-21 21:23:52 +0200

    Added firebase support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyD8LtSVISHxzWhrigcXJgv5j-6oc0s-dTI',
+            authDomain: 'auth-cbdb7.firebaseapp.com',
+            databaseURL: 'https://auth-cbdb7.firebaseio.com',
+            projectId: 'auth-cbdb7',
+            storageBucket: 'auth-cbdb7.appspot.com',
+            messagingSenderId: '211557195790',
+            appId: '1:211557195790:web:f9b4fe9b0093893d'
+          })
+    }
+
     render() {
         return (
             <View>
